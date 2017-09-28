37d184b531486d522c1e568f863b11d2d7d1a8ff
Chathan Driehuys <cdriehuys@gmail.com>
2017-09-28 15:58:34 -0400

    Configure webpack.

---
+++ webpack.config.babel.js
@@ -0,0 +1,32 @@
+import path from 'path';
+
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import HTMLWebpackTemplate from 'html-webpack-template';
+
+
+const BUILD_DIR = path.resolve(__dirname, 'dist');
+const SOURCE_DIR = path.resolve(__dirname, 'src');
+
+
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  appMountId: 'app',
+  filename: path.resolve(BUILD_DIR, 'index.html'),
+  inject: false,
+  mobile: true,
+  template: HTMLWebpackTemplate,
+  title: 'UltiManager',
+});
+
+
+export default {
+  devServer: {
+    historyApiFallback: true,
+    port: 8000,
+  },
+  entry: path.resolve(SOURCE_DIR, 'index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: BUILD_DIR,
+  },
+  plugins: [HTMLWebpackPluginConfig],
+};
