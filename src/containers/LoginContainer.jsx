bc9ca623e61ae5fe7a40a9897ea330612dbf3fdd
Chathan Driehuys <chathan@driehuys.com>
2017-09-28 18:28:38 -0400

    Connect login container to form.
    The container now has a handler for the submit event from the login
    form.

--- src/containers/LoginContainer.jsx
+++ src/containers/LoginContainer.jsx
@@ -3,12 +3,26 @@ import React from 'react';
 import LoginForm from '../components/LoginForm';
 
 
-const LoginContainer = () => (
-  <div>
-    <h1>Log In</h1>
-    <LoginForm />
-  </div>
-);
+class LoginContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin() { // eslint-disable-line class-methods-use-this
+    console.log('Log in form submitted.');
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Log In</h1>
+        <LoginForm onSubmit={this.handleLogin} />
+      </div>
+    );
+  }
+}
 
 
 export default LoginContainer;
